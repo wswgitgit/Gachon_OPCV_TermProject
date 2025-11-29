891f6a067fd89f9718f2b9104354607d694293b7
rladpwns03-beep <rladpwns03@gachon.ac.kr>
2025-11-29 14:28:46 +0900

    Enhance transcription with noise reduction settings
    Add parameters to transcribe method for noise reduction.

--- stt_module.py
+++ stt_module.py
@@ -5,6 +5,9 @@ def run_stt(audio_path: str):
     #load_model 메소드의 파라미터는 변환 모델의 종류를 의미
     #다른 모델도 존재하지만 아직 시험하지 않았음.
     model = whisper.load_model("base")  
-    result = model.transcribe(audio_path)
+    #실제 음성이 아닌 바람과 같은 노이즈를 제거하기 위한 코드입니다. 
+    result = model.transcribe(audio_path, 
+                              temperature=0.0, 
+                              no_speech_threshold=0.4)
     #반환 값의 key 부분인 "segments"은 건드리시면 안됩니다.
     return result["segments"]
