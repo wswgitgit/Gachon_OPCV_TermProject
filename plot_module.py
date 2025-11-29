b34e18bb7485a35b3a9f5aa9e22d9c821ba4d66c
rladpwns03-beep <rladpwns03@gachon.ac.kr>
2025-11-29 14:10:51 +0900

    Configure matplotlib font settings for Korean support
    Add font configuration for matplotlib to support Korean characters.

--- plot_module.py
+++ plot_module.py
@@ -8,7 +8,19 @@ os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 #그래프를 출력하는 메소드입니다.
 
 import matplotlib.pyplot as ppl
+from matplotlib import font_manager
 from config import SAVE_PATH
+
+#김예준: 윈도우를 쓰시는 분들은 아래의 코드를 작성해 plot의 글꼴이 깨지지 않게 하세요. 
+font_path = "C:\Windows\Fonts\malgun.ttf"
+if os.path.exists(font_path):
+    font_name = font_manager.FontProperties(fname=font_path).get_name()
+    ppl.rcParams["font.family"] = font_name
+else:
+    print("[경고] 지정한 한글 폰트를 찾지 못했습니다. font_path를 확인하세요:", font_path)
+
+# 마이너스 기호 깨짐 방지
+ppl.rcParams["axes.unicode_minus"] = False
 
 def plot_timeline_scores(minute_scores: dict, save_path: str = SAVE_PATH):
     minutes = sorted(minute_scores.keys())
